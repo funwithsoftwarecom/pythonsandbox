05b26cbc09dcd37dd9f166c984bb94a113ad9154
Tom Moody <tom.moody@gmail.com>
2019-04-18 11:50:37 -0600

    This implements the calculate_payment function in loantools.py

--- loantools.py
+++ loantools.py
@@ -3,3 +3,29 @@ Created on Apr 18, 2019
 
 @author: tom
 '''
+
+
+
+'''
+This method takes in the beginning principal of a loan, the annual percentage rate (aka APR), and the number
+of payment periods.  The annual_percentage_rate should be entered in standard format and the method will convert 
+to decimal.  So if the annual percentage is 7.5 then enter it that way and the function will /100.
+'''
+def calculate_payment(beginning_principal,annual_percentage_rate,payment_periods):
+    payment = 0.00
+    
+    '''  This is the rate per payment period.  
+         Since the rate is annual (aka APR) we divide by 12.'''
+    r = (annual_percentage_rate/100)/12
+
+    numerator = r*(1+r)**payment_periods
+    denominator = ((1+r)**payment_periods)-1
+    
+    payment = beginning_principal*(numerator/denominator) 
+    
+    return payment
+
+
+
+print(calculate_payment(20000, 7.5, 60)) 
+print("Done")
